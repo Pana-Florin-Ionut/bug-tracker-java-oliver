90f892117254e1eb8ea0959d4f4cd7544c97b78a
Olivér <oliver.mrakovics@gmail.com>
2021-08-31 10:23:57 +0200

    The project readme now supports the HTML syntax.

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,2 +1,7 @@
 INSERT INTO roles(role) VALUES ('EMPLOYEE');
 INSERT INTO roles(role) VALUES ('ADMIN');
+
+INSERT INTO users (fname, lname, email, password, image) VALUES ('First', 'Last', 'user@gmail.com', '$2a$10$et.spcFiyH3XTx0.8SvfX.bSvBw61wG8YpgY5Mdus9QbVZYOOh6vO', '/assets/images/faces/face15.jpg');
+
+INSERT INTO users_roles (user_id, role_id) VALUES (1, 1);
+INSERT INTO users_roles (user_id, role_id) VALUES (1, 2);
